042e99487cb73dcbb65e2ec7f64de3837c565e8d
Aleksandra-Barysheva <aleksandra.barysheva16@gmail.com>
2023-06-04 23:17:48 +0300

    DS-1.1 DQC and ETL
    	Changes:
    	- added detailed analysis of negative values
    	- provided algorithm for cleaning out errors from return values
    	- added shop and item analysis
    	- provided some aggregation ideas for item names
    	- added map_pipes for shop_id, item_id and item_name in the report
    	- added map_pipes to etl.py

--- src/etl/etl.py
+++ src/etl/etl.py
@@ -8,30 +8,42 @@ from .src.transform import Transform
 RELEVANT_TABLES = ["sales_train",
                    "items",
                    "item_categories",
-                   "shops"]
+                   "shops",
+                   "test"]
 
 
 class RelationsTransform(Transform):
-    def __init__(self, filter_pipes=tuple()):
+    def __init__(self, map_pipes=tuple(), filter_pipes=tuple(),):
+        self.map_pipes = map_pipes
         self.filter_pipes = filter_pipes
 
-    def apply_filter(self, relations):
-        for key in relations:
-            if key in self.filter_pipes:
-                pipe = self.filter_pipes[key]
-                relations[key] = self._apply_pipe(relations[key], pipe)
-        return relations
-
     def apply_map(self, relations):
-        return relations
+        return self._apply_pipes(relations, self.map_pipes)
+
+    def apply_filter(self, relations):
+        return self._apply_pipes(relations, self.filter_pipes)
 
     def apply_reduce(self,  relations):
-        merged_relations = (relations["sales_train"]
-                            .merge(relations["items"], on='item_id', how='left')
-                            .merge(relations["item_categories"], on='item_category_id', how='left')
-                            .merge(relations["shops"], on='shop_id', how='left')
-                            )
-        return merged_relations
+        merged_relations_train = (relations["sales_train"]
+                                  .merge(relations["items"], on='item_id', how='left')
+                                  .merge(relations["item_categories"], on='item_category_id', how='left')
+                                  .merge(relations["shops"], on='shop_id', how='left')
+                                  )
+
+        merged_relations_test = (
+            relations["test"]
+            .merge(relations["items"], on='item_id', how='left')
+            .merge(relations["item_categories"], on='item_category_id', how='left')
+            .merge(relations["shops"], on='shop_id', how='left')
+        )
+        return merged_relations_train, merged_relations_test
+
+    def _apply_pipes(self, relations, pipe):
+        for key in relations:
+            if key in pipe:
+                pipe = pipe[key]
+                relations[key] = self._apply_pipe(relations[key], pipe)
+        return relations
 
     def _apply_pipe(self, df, pipe):
         for f in pipe:
@@ -48,6 +60,7 @@ class ETL:
         return self._get_relations()
 
     def load(self):
+        "returns (train, test) tuple"
         relations = self.extract()
         relations = self.transform(relations)
         return relations
